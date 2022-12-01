5208d03fbf72156cc567bb6939b084bcef1a1672
Bison Hubert <bisonhubert@gmail.com>
2022-12-01 01:14:49 -0800

    Add new day script

---
+++ new_day.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+cp templates/runner.py runner_$1.py
+cp templates/input.py input_$1.py
---
+++ templates/runner.py
@@ -0,0 +1,9 @@
+from tests import run_star_test, run_unit_test
+
+# Code goes here
+
+# Tests
+# describe unit test
+run_unit_test(1, 0)
+# solution x: <prompt-from-website>
+run_star_test(1, 0)
