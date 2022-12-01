12e694a8a7042eb5774318b9f10e417fe40cc740
Bison Hubert <bisonhubert@gmail.com>
2022-12-01 01:16:05 -0800

    Add warning

--- new_day.sh
+++ new_day.sh
@@ -1,4 +1,8 @@
 #/bin/bash
 
+# CAUTION: THIS SCRIPT WILL OVERWRITE EXISTING FILES
+# todobison Run a check and warn if you are going to overwrite a file.
+# Otherwise you will need to rely on version control to revert overwrites.
+
 cp templates/runner.py runner_$1.py
 cp templates/input.py input_$1.py
