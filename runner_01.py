5a2d0fc4d7088fc636983ca5e0420f6a0f16f8a0
Bison Hubert <bisonhubert@gmail.com>
2022-12-01 00:49:58 -0800

    Day 1 second star

--- runner_01.py
+++ runner_01.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 INPUT_FILEPATH = './input_01.py'
 
 def get_inventory(filepath):
@@ -15,22 +17,34 @@ def get_inventory(filepath):
             elf_inventory.append(ration)
     return inventory
 
-def get_calorie_totals(inventory):
-    calorie_totals = []
-    for elf_inventory in inventory:
-        calorie_totals.append(sum(elf_inventory))
-    return calorie_totals
+def get_inventory_details(inventory):
+    inventory_details = []
+    for indx, elf_inventory in enumerate(inventory):
+        inventory_details.append(sum(elf_inventory))
+    return inventory_details
 
 
 inventory = get_inventory(INPUT_FILEPATH)
-calorie_totals = get_calorie_totals(inventory)
-most_calories = max(calorie_totals)
+inventory_details = get_inventory_details(inventory)
+most_calories = max(inventory_details)
 
+elf_count = 3
+top_3_most_calories = 0
+inventory_details_copy = inventory_details[:]
+# import pdb;pdb.set_trace()
+for i in list(range(3)):
+    max_calories = max(inventory_details_copy)
+    top_3_most_calories += max_calories
+    # print('top 3 most', top_3_most_calories)
+    inventory_details_copy.remove(max_calories)
+
+# Tests
 # inventory is splitting correctly
-print('💚' if inventory[0] == [7769, 6798, 11685, 10826, 11807, 5786, 7932] else '❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌')
-
+print('💚' if inventory[0] == [7769, 6798, 11685, 10826, 11807, 5786, 7932] else '❌')
 # calorie totals returns the correct sum of the first inventory in the test above
-print('💚' if calorie_totals[0] == 62603 else '❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌')
-
+print('💚' if inventory_details[0] == 62603 else '❌')
 # solution 1: Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
-print('⭐️' if most_calories == 71780 else '❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌') # first try 🙌
+print('⭐️' if most_calories == 71780 else '❌') # first try 🙌
+# solution 2: Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+# attempt_1 = 215340, too high
+print('⭐️' if top_3_most_calories == 212489 else '❌')
