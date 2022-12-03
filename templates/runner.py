a09dff9ba4879a5cad59058b38c539bb7b5f81a4
Bison Hubert <bisonhubert@gmail.com>
2022-12-02 18:18:41 -0800

    Link input file

--- templates/runner.py
+++ templates/runner.py
@@ -1,6 +1,29 @@
+import os
+import sys
+
 from tests import run_star_test, run_unit_test
 
+def get_input_filepath(filename):
+    day = filename.split('/')[-1].split('_')[-1].split('.')[0]
+    return f'./input_{day}.py'
+
+def parse_input(filepath):
+    # i/o
+    f = open(filepath)
+    readlines = f.readlines()
+    # parse input
+    return readlines
+
+def get_input():
+    current_filename = os.path.abspath(sys.argv[0])
+    INPUT_FILEPATH = get_input_filepath(current_filename)
+    return parse_input(INPUT_FILEPATH)
+
 # Code goes here
+input = get_input()
+
+# Stdout here
+print('input', input)
 
 # Tests
 # describe unit test
