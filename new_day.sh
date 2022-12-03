ab2edca96cd72614fa6f49112d93d8bfe79ed547
Bison Hubert <bisonhubert@gmail.com>
2022-12-03 02:07:54 -0800

    Test input link when making new day

--- new_day.sh
+++ new_day.sh
@@ -4,5 +4,6 @@
 # todobison Run a check and warn if you are going to overwrite a file.
 # Otherwise you will need to rely on version control to revert overwrites.
 
-cp templates/runner.py runner_$1.py
-cp templates/input.py input_$1.py
+cp templates/runner.py runners/runner_$1.py
+cp templates/input.py inputs/input_$1.py
+python runners/runner_$1.py
