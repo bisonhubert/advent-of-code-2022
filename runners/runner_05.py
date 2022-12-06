59488a95e7238b90a31baf2bee2b3f88237dd4bd
Bison Hubert <bisonhubert@gmail.com>
2022-12-05 23:08:25 -0800

    Format first line input

--- runners/runner_05.py
+++ runners/runner_05.py
@@ -1,3 +1,5 @@
+from itertools import groupby
+
 import os
 import sys
 
@@ -7,12 +9,29 @@ def get_input_filepath(filename):
     day = filename.split('/')[-1].split('_')[-1].split('.')[0]
     return f'./inputs/input_{day}.txt'
 
+def format_stacks(stack):
+    formatted_stack = ''
+    crate_top = 0
+    for i in list(range(len(stack))):
+        if (i + 1) % 4 == 0:
+            crate = stack[crate_top:i+1]
+            if '[' in crate and ']' in crate:
+                formatted_stack += crate.replace('[', '').replace(']', '').replace(' ', '')
+                crate_top += i + 1
+            else:
+                formatted_stack += '*'
+            print('crate', crate)
+            print('formatted_stack', formatted_stack)
+    print('hi')
+    return formatted_stack
+
 def parse_input(filepath):
-    # i/o
     f = open(filepath)
     readlines = f.readlines()
-    # parse input
-    return readlines
+    stacks = readlines[:readlines.index('\n')]
+    # stacks = format_stacks(readlines[:readlines.index('\n')])
+    procedure = [readline.strip() for readline in readlines[readlines.index('\n')+1:]]
+    return stacks, procedure
 
 def get_input():
     current_filename = os.path.abspath(sys.argv[0])
@@ -20,14 +39,19 @@ def get_input():
     return parse_input(INPUT_FILEPATH)
 
 # Code goes here
-input = get_input()
+stacks, procedure = get_input()
+import pdb;pdb.set_trace()
 
 # Stdout here
-print('--')
-print('input', input)
+# print('--')
+# print('stacks', stacks)
+# print('procedure', procedure)
 
 # Tests
 # describe unit test
-run_unit_test(1, 0)
+output_1 = format_stacks(stacks[0])
+import pdb;pdb.set_trace()
+run_unit_test(output_1, 'N*Q**N***')
+# run_unit_test(top_crates_test_output, 'NHQQFNMJS')
 # solution x: <prompt-from-website>
-run_star_test(1, 0)
+# run_star_test(1, 0)
